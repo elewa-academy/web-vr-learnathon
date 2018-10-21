c8d7b95201a0453c1e26cde9618aeb61947beef3
Tobias Busch <tbusch@cochlear.com>
2018-10-21 17:08:45 +0200

    added key

--- prison-break/birthdaygreeting-component.js
+++ prison-break/birthdaygreeting-component.js
@@ -1,14 +1,31 @@
+// AFRAME.registerComponent('birthdaygreeting', {
+//   init: function() {
+//     // let backgroundmusic = document.querySelector("#soundtrack");
+//     // let audio = document.querySelector("#happy");
+//     let played = false;
+
+//     this.el.addEventListener('click', () => {
+//       console.log("hello again!")
+//       if (!played) {
+//         let audio = document.querySelector("#happy");
+//         audio.play();
+//         played = false;
+//       }
+//     });
+//   }
+// })
+
+
 AFRAME.registerComponent('birthdaygreeting', {
   init: function() {
-    let backgroundmusic = document.querySelector("#soundtrack");
-    let audio = document.querySelector("#happy");
+    // let audio = document.querySelector("#outsidesounds");
     // audio.play();
-    let played = false;
-    this.el.addEventListener('mouseenter', () => {
-      if (!played) {
+    let playing = false;
+    this.el.addEventListener('click', () => {
+      if (!playing) {
         let audio = document.querySelector("#happy");
         audio.play();
-        played = true;
+        playing = true;
       }
     });
   }
